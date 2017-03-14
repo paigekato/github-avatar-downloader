0ba450f96386fe21958d63bbd2c5a04d3ebf40b9
Paige <paigekato@gmail.com>
2017-03-14 05:02:42 +0000

    linting fixes

--- download_avatars.js
+++ download_avatars.js
@@ -8,28 +8,29 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = "https://" + GITHUB_USER + ":" + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + "/" + repoName + "/contributors";
   var options = {
-  url: requestURL,
-  headers: {
-    'User-Agent': 'paigekato'
-   }
- };
-   if(repoOwner && repoName) {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'paigekato'
+    }
+  };
+  if(repoOwner && repoName) {
     request(options, function(err, response, body) {
-      if(err) throw err;
+      if(err)
+        throw err;
       console.log('Response Status Code: ', response.statusCode);
       var obj = JSON.parse(body);
       cb(null, obj);
-      });
+    });
 
   } else {
-  console.log("Please enter a repo owner and name.");
+    console.log("Please enter a repo owner and name.");
   }
 }
 
 function downloadImageByURL(url, filePath) {
   request.get(url)
     .on("error", function(err) {
-    throw err;
+      throw err;
     })
   .on("response", function(response) {
     console.log("Response Status Code: ", response.statusCode);
@@ -41,9 +42,9 @@ getRepoContributors(process.argv[2], process.argv[3], function(err, result) {
   console.log("Errors", err);
   console.log("Result", result);
 
-   result.forEach(function(element) {
-     var filePath = "./avatars/" + element.login + ".jpg";
-     downloadImageByURL(element.avatar_url, filePath);
+  result.forEach(function(element) {
+    var filePath = "./avatars/" + element.login + ".jpg";
+    downloadImageByURL(element.avatar_url, filePath);
   });
 });
 
